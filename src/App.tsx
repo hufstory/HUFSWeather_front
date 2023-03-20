bd68c8bbbefb1f6f467afb8a609d333ff1583628
Kyoeun Lee <lke159357@hufs.ac.kr>
2023-03-20 13:36:47 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import "./styles/tailwind.css"
+
+function App() {
+  return (
+    <div className=" text-pink-300   bg-black">
+
+        
+          Learn React
+        
+    </div>
+  );
+}
+
+export default App;
