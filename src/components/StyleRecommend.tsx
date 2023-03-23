13b6e1b3ab4f23d6d26e33441c7047ca8cd5bc86
Kyoeun Lee <lke159357@hufs.ac.kr>
2023-03-23 15:45:43 +0900

    FEAT: 폰트 추가

--- src/components/StyleRecommend.tsx
+++ src/components/StyleRecommend.tsx
@@ -1,7 +1,7 @@
 import React from "react"
 
 const StyleRecommend = () => {
-  return <h1>StyleRecommend</h1>
+  return <h1 className="font-GangwonBold">StyleRecommend</h1>
 }
 
 export default StyleRecommend
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,15 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
 
+  theme: {
+    fontFamily: {
+      GangwonBold: ["GangwonEduPowerExtraBoldA", "sans-serif"],
+      GmarketMedium: ["GmarketSansMedium", "sans-serif"],
+      GmarketLight: ["GmarketSansLight", "sans-serif"],
+      Jalnan: ["Jalnan", "sans-serif"],
+    },
+  },
+
   variants: {
     extends: {},
   },
