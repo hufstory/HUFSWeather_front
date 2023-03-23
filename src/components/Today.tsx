29ff8516e89331ec6d32fe35e82b0c8f74cd9a67
Kyoeun Lee <lke159357@hufs.ac.kr>
2023-03-24 02:31:43 +0900

    Chore: package.json style.css 빌드

--- src/components/Today.tsx
+++ src/components/Today.tsx
@@ -1,9 +1,16 @@
 import React from "react"
 
 const Today = () => {
-  return (
-    <h1>Today</h1>
-  )
-}
+ 
 
-export default Today
+
+  return (
+    <div>
+      <h1 className="text-orange-500">오늘의 캠퍼스</h1>
+ 
+    </div>
+       
+  );
+};
+
+export default Today;
