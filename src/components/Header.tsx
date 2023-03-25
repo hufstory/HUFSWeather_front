38151213d21c31333eb66ba2ecabfd6c9d04bb8a
DongHyeok Lim <jh07050@gmail.com>
2023-03-25 21:42:36 +0900

    Style: header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,15 +5,15 @@ const Header = () => {
   return (
     <header className="sticky inset-x-0 top-0 z-50 left-0 bg-sky-900 text-White">
       <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-        <span className="md:ml-5 text-2xl text-white">HUFSWeather</span>
+        <span className="md:ml-5 text-2xl text-white ">외상청 | 한국외대 기상청</span>
+        <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
+          <Link to="/aboutpage" className="mr-5 text-white hover:text-red-400">
+            외상청이란?
+          </Link>
+        </nav>
       </div>
-      <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
-        <Link to="/aboutpage" className="mr-5 text-white hover:text-red-400">
-          외상청이란?
-        </Link>
-      </nav>
     </header>
-  );
+  )
 };
 
 export default Header;
