f0087278dae0db61fa123f6b1cacac7a9a396a3d
DongHyeok Lim <jh07050@gmail.com>
2023-03-26 18:08:17 +0900

    Style: header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,9 +3,9 @@ import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <header className="sticky inset-x-0 top-0 z-50 left-0 bg-sky-900 text-White">
-      <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-        <span className="md:ml-5 text-2xl text-white ">외상청 | 한국외대 기상청</span>
+    <header className="sticky mx-0 top-0 bg-sky-800 text-White">
+      <div className="container h-[72px] mx-auto flex flex-wrap flex-col md:flex-row items-center">
+        <span className="text-2xl text-white ">외상청 | 한국외대 기상청</span>
         <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
           <Link to="/aboutpage" className="mr-5 text-white hover:text-red-400">
             외상청이란?
