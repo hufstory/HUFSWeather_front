cad7863d3517681ce3d5b585e40418b4fc63712f
DongHyeok Lim <jh07050@gmail.com>
2023-03-26 15:57:26 +0900

    Feat: Today 오늘 날짜 추가

--- src/components/Today.tsx
+++ src/components/Today.tsx
@@ -1,16 +1,21 @@
 import React from "react"
 
 const Today = () => {
- 
-
+  const today: Date = new Date()
+  const dayOfWeek: string[] = ["일", "월", "화", "수", "목", "금", "토"]
+  const month: number = today.getMonth() + 1
+  const date: number = today.getDate()
+  const day: string = dayOfWeek[today.getDay()]
 
   return (
     <div>
       <h1 className="text-orange-500">오늘의 캠퍼스</h1>
- 
+      <h1>오늘의 외상청</h1>
+      <h1>
+        {month}월 {date}일 {day}요일 소식입니다.
+      </h1>
     </div>
-       
-  );
-};
+  )
+}
 
-export default Today;
+export default Today
