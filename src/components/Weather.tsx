daeecf782c685ee2f56bb5f1123876517b8a089d
DongHyeok Lim <jh07050@gmail.com>
2023-03-29 22:05:33 +0900

    Feat: API 받아오는 Axios기반 코드 작성

--- src/components/Weather.tsx
+++ src/components/Weather.tsx
@@ -1,7 +1,43 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
+import axios from "axios"
 
 const Weather = () => {
-  return <h1>Weather</h1>
+
+  // TypeScript 특징 상으로 현재 백엔드가 JS로 구성되어있어서 직접 타입 설정
+  const [data, setData] = useState<{
+    temp: number
+    feel_temp: number
+    humidity: number
+    weather: string
+  } | null>(null)
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get("http://localhost:3001") // 백엔드한테서 포트 3001번 주소로 받아올 예정
+        console.log(response.data[0])
+        setData(response.data[0])
+      } catch (error) {
+        console.error(error)
+      }
+    }
+    fetchData()
+  }, [])
+
+  return (
+    <div>
+      {data ? (
+        <>
+          <p className="text-red">Temperature: {data.temp}</p>
+          <p>Feels like: {data.feel_temp}</p>
+          <p>Humidity: {data.humidity}</p>
+          <p>Weather: {data.weather}</p>
+        </>
+      ) : (
+        <p>Loading...</p>
+      )}
+    </div>
+  )
 }
 
 export default Weather
