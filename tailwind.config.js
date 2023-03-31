821dc7a957675624b2e86061cde72439f8b937aa
DongHyeok Lim <jh07050@gmail.com>
2023-03-31 18:45:13 +0900

    Style: boxShadow 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,11 @@ module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
 
   theme: {
+    extend: {
+      boxShadow: {
+        default: "4px 12px 30px 6px rgba(0, 0, 0, 0.09)",
+      },
+    },
     fontFamily: {
       GangwonBold: ["GangwonEduPowerExtraBoldA", "sans-serif"],
       GmarketMedium: ["GmarketSansMedium", "sans-serif"],
