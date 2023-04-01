460f8ef20fb9dd69ddf94d77f575cdd21b695a85
DongHyeok Lim <jh07050@gmail.com>
2023-04-01 18:54:46 +0900

    Add: Week component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import AboutPage from './pages/AboutPage';
 import MainPage from './pages/MainPage';
 import "./styles/tailwind.css"
 
@@ -8,6 +9,7 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<MainPage />} />
+        <Route path="/aboutpage" element={<AboutPage />} />
       </Routes>
     </Router>
   )
