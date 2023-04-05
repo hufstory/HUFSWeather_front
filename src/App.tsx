6416be2697656afddcd1db48b540676d8791944e
woongbishin <kipper12335@gmail.com>
2023-04-05 20:40:47 +0900

    upload branch

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,24 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
-import AboutPage from './pages/AboutPage';
-import MainPage from './pages/MainPage';
-import "./styles/tailwind.css"
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import AboutPage from "./pages/AboutPage";
+import MainPage from "./pages/MainPage";
+import "./styles/tailwind.css";
+import { Helmet } from "react-helmet";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<MainPage />} />
-        <Route path="/aboutpage" element={<AboutPage />} />
-      </Routes>
-    </Router>
-  )
+    <div>
+      <Helmet>
+        <title>외상청 | 한국외대 기상청</title>
+      </Helmet>
+      <Router>
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+          <Route path="/aboutpage" element={<AboutPage />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
