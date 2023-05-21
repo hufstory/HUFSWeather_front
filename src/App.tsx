80af6dc17cd4d37f620a4c72a527a1b20c2eee12
DongHyeok Lim <jh07050@gmail.com>
2023-05-21 20:59:48 +0900

    Design: 브라우저 기본 시맨틱 태그 설정 해제 및 CSS 수정

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,13 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Helmet } from "react-helmet";
 import { MainPage } from "./pages/MainPage/MainPage";
+import { Reset } from "styled-reset";
+
 
 function App() {
   return (
     <div>
+      <Reset />
       <Helmet>
         <title>외상청 | 한국외대 기상청</title>
       </Helmet>
